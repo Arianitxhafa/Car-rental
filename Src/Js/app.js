23405f021c1987b00d08ab1840190d0ba6274403
Arianit Xhafa <arianit.xhafa@umib.net>
2026-03-17 15:23:10 +0100

    Permiresimi i app.js

--- Src/Js/app.js
+++ Src/Js/app.js
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Car Rent</title>
-<link rel="stylesheet" href="../src/css/style.css">
-</head>
+document.getElementById("bookingForm")?.addEventListener("submit", function(e){
 
-<body>
+e.preventDefault()
 
-<header>
-<h1>🚗 Car Rent</h1>
-<nav>
-<a href="index.html">Home</a>
-<a href="cars.html">Cars</a>
-<a href="booking.html">Booking</a>
-</nav>
-</header>
+alert("Your car has been booked successfully!")
 
-<section class="hero">
-<h2>Rent Your Dream Car</h2>
-<p>Simple car rental application </p>
-</section>
-
-</body>
-</html>
+})
